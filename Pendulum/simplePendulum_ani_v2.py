536c15cc162e28f65d0a18f29b1135468e17f230
knakaji <knakaji@mac.titech.ac.jp>
2023-05-14 17:52:36 +0900

    modified

--- Pendulum/simplePendulum_ani_v2.py
+++ Pendulum/simplePendulum_ani_v2.py
@@ -11,14 +11,15 @@ def simplePendulum(s, t):
     dsdt = [dtheta, -(g/l)*np.sin(theta)]
     return dsdt
 
-theta0 = np.pi/4                # [m]   initial angle
+theta0 = np.pi/4        # [m]   initial angle
 l = 1                   # [m] length of pendulum 
-v0 = 1                    # [m/s] initial velocity
-s0 = [theta0, v0/l]             # initial condition
-t = 10                      # [s] duration time
-dt = 0.05                    # [s] interval time
+v0 = 1                  # [m/s] initial velocity
+s0 = [theta0, v0/l]     # initial condition
+t = 10                  # [s] duration time
+dt = 0.05               # [s] interval time
 
 t = np.arange(0, t+dt, dt)
+print(len(t))
 
 sol = odeint(simplePendulum, s0, t)
 theta = sol[:,0]
@@ -30,25 +31,29 @@ ax = fig.add_subplot(111, aspect='equal', xlim=[-l, l], ylim=(-1.2*l, l))
 ax.grid()
 
 line, = ax.plot([], [], 'ro-', markersize='8', animated=True)
+# ここでは[],[]としているが、下でline.set_data([0, l + x[i]], [0, 0])で実際の値を入れている
 
 time_template = 'time = %.1fs'
 time_text = ax.text(0.1, 0.9, '', transform=ax.transAxes)
+# ここでは''としているが、下で time_text.set_textで実際のテキストを入れている
+
 
 def init():
     time_text.set_text('')
     return line, time_text
 
-def update(i):
+def update(i):      # ここのiは下のframes=fに対応した引数になっている
     next_x = [0, x[i]]
     next_y = [0, y[i]]
     line.set_data(next_x, next_y)
     time_text.set_text(time_template % (i*dt))
     return line, time_text
 
+f = np.arange(0, len(t))    # number of frames
 frame_int = 1000 * dt       # [ms] interval between frames
 fps = 1000/frame_int        # frames per second
 
-ani = FuncAnimation(fig, update, frames=np.arange(0, len(t)),
+ani = FuncAnimation(fig, update, frames=f,
                     init_func=init, blit=True, interval=frame_int, repeat=True)
 plt.show()
 
