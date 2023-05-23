f2ae56b12da320f49bb1f14fe86f9132f316c2fa
knakaji <knakaji@mac.titech.ac.jp>
2023-05-24 00:05:02 +0900

    modified

--- coupledHarmonicOsci/coupledHarmonicOsci_ani_trans.py
+++ coupledHarmonicOsci/coupledHarmonicOsci_ani_trans.py
@@ -1,69 +1,94 @@
+# ordinary differential equation of coupled harmonic oscillator (transverse)
+
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def harmonicOscillator(s, t, k1, k2, m):
+def coupledHarmonicOscillator(s, t, k1, k2, m):
 
-    x1, v1, x2, v2 = s
+    x1, v1, x2, v2 = s              # s = (x1, v1, x2, v2)
+    a1 = - (k1+k2)*x1/m + k2*x2/m   
     a1 = - (k1+k2)*x1/m + k2*x2/m
     a2 = k2*x1/m - (k1+k2)*x2/m
     dsdt = [v1, a1, v2, a2]
     return dsdt
 
-k1 = 10                    # [N/m] spring constant
+# variables
+k1 = 10                     # [N/m] spring constant
 k2 = 50                     # [N/m] spring constant
 m = 10                      # [kg] mass
 l1 = 20                     # [m] equilibrium length
 l2 = 20                     # [m] equilibrium length
 l3 = 20                     # [m] equilibrium length
 L = l1 + l2 + l3
-x1_0 = 5                    # [m]   initial position
-x2_0 = 5                    # [m]   initial position
-v1_0 = 0                    # [m/s] initial velocity
-v2_0 = 0                    # [m/s] initial velocity
-s0 = [x1_0, v1_0, x2_0, v2_0]
 af1 = np.sqrt(k1/m)         # angular frequency   
-af2 = np.sqrt((k1+2*k2)/m)  # angular frequency   
-t = 8*np.pi/af1             # [s] duration time
-dt = 0.1                    # [s] interval time
+af2 = np.sqrt((k1+2*k2)/m)  # angular frequency
+period = 2*np.pi/af1        # period of af1[s] (T)   
+t = 4*period                # [s] duration time
+dt = 0.05                    # [s] interval time
+
+# initial condition
+try:
+    x1_0 = int(input('initial position of mass1 (default=5): '))
+except ValueError:
+    x1_0 = 5
+try:
+    x2_0 = int(input('initial position of mass2 (default=10): '))
+except ValueError:
+    x2_0 = 10
+try:
+    v1_0 = int(input('initial velocity of mass1 (default=0): '))
+except ValueError:
+    v1_0 = 0
+try:
+    v2_0 = int(input('initial velocity of mass2 (default=0): '))
+except ValueError:
+    v2_0 = 0
+
+s0 = [x1_0, v1_0, x2_0, v2_0]   # initial condition
 
 t = np.arange(0, t+dt, dt)
 
-sol = odeint(harmonicOscillator, s0, t, args=(k1, k2, m))
-x1, x2 = sol[:, 0], sol[:, 2]
-q1 = (1/np.sqrt(2))*(x1 + x2)
-q2 = (1/np.sqrt(2))*(-x1 + x2)
+sol = odeint(coupledHarmonicOscillator, s0, t, args=(k1, k2, m))  # ODEの解を求めている
+x1, x2 = sol[:, 0], sol[:, 2]   # [x1], [x2]が出てくる
+q1 = (1/np.sqrt(2))*(x1 + x2)   # normal mode q1
+q2 = (1/np.sqrt(2))*(-x1 + x2)  # normal mode q2
 
 fig = plt.figure()
-ax = fig.add_subplot(111, xlabel='x position [m]', xlim=(0, L), ylim=(-L/4, L))
+ax = fig.add_subplot(111, xlim=(0, L), ylim=(-L/4, L))
 ax.grid()
+ax.set_axisbelow(True)
+ax.set_xlabel('$x$ position [m]')
 
-line1, = plt.plot([], [], 'ro-', animated=True)
-line2, = plt.plot([], [], 'bo-', animated=True)
-line3, = plt.plot([], [], 'go-', animated=True)
+line, = plt.plot([], [], 'ro-', animated=True)
+norm1, = plt.plot([], [], 'bo-', animated=True)
+norm2, = plt.plot([], [], 'go-', animated=True)
+# ここでは[],[]としているが、下でlinei.set_dataで実際の値を入れている
 
 time_template = 'time = %.1fs'
 time_text = ax.text(0.1, 0.9, '', transform=ax.transAxes)
+# また、ここでは''としているが、下で time_text.set_textで実際のテキストを入れている
 
-def init():
+def init():               # FuncAnimationでinit_funcで呼び出す
     time_text.set_text('')
-    return line1, line2, line3, time_text
-#    return line1, time_text
+    return line, norm1, norm2, time_text
 
 def update(i):              # ここのiは下のframes=np.arange(0, len(t))に対応した引数になっている
-    line1.set_data([0, l1, l1 + l2, L], [0, x1[i], x2[i], 0])
-    line2.set_data([0, L/2 + q1[i]], [20, 20])
-    line3.set_data([0, L/2 + q2[i]], [40, 40])
+    line.set_data([0, l1, l1 + l2, L], [0, x1[i], x2[i], 0])
+    norm1.set_data([0, L/2 + q1[i]], [20, 20])
+    norm2.set_data([0, L/2 + q2[i]], [40, 40])
     time_text.set_text(time_template % (i*dt))
-    return line1, line2, line3, time_text
-#    return line1, time_text
+    return line, norm1, norm2, time_text
 
+f = np.arange(0, len(t))
 frame_int = 1000 * dt       # [ms] interval between frames
 fps = 1000/frame_int        # frames per second
 
-ani = FuncAnimation(fig, update, frames=np.arange(0, len(t)),
+ani = FuncAnimation(fig, update, frames=f,
                     init_func=init, blit=True, interval=frame_int, repeat=True)
-plt.show()
 
-ani.save('./gif/coupledVibration_v2.gif', writer='pillow', fps=fps)
+savefile = './gif/coupledHarmonicOsci_transverse_(x0={0},v0={1}, x1={2},v1={3}).gif'.format(x1_0,v1_0,x2_0,v2_0)
+ani.save(savefile, writer='pillow', fps=fps)
+
+plt.show()
