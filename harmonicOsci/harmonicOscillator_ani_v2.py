f8fbfae0a78907937609a6ce1187bf0dc87bcced
knakaji <knakaji@mac.titech.ac.jp>
2023-05-26 22:18:05 +0900

    harmonic

--- harmonicOsci/harmonicOscillator_ani_v2.py
+++ harmonicOsci/harmonicOscillator_ani_v2.py
@@ -54,8 +54,8 @@ ax.set_xlabel('$x$ position [m]')
 line, = plt.plot([], [], 'ro-', animated=True)
 # ここでは[],[]としているが、下でline.set_data([0, l + x[i]], [0, 0])で実際の値を入れている
 
-variable_template = r'$k$ = {0:.1f} N/m, $m$ = {1:.1f} kg'.format(k,m)
-variable_text = ax.text(0.6, 0.9, '', transform=ax.transAxes) # 図形の枠を基準にした位置にテキストが挿入
+var_template = r'$k$ = {0:.1f} N/m, $m$ = {1:.1f} kg'.format(k,m)
+var_text = ax.text(0.6, 0.9, '', transform=ax.transAxes) # 図形の枠を基準にした位置にテキストが挿入
 
 period_template = '$T$ = %.2f s'
 period_text = ax.text(0.1, 0.8, '', transform=ax.transAxes) # 図形の枠を基準にした位置にテキストが挿入
@@ -67,8 +67,8 @@ time_text = ax.text(0.1, 0.9, '', transform=ax.transAxes) # 図形の枠を基�
 def init():                 # FuncAnimationでinit_funcで呼び出す
     time_text.set_text('')
     period_text.set_text('')
-    variable_text.set_text('')
-    return line, time_text, period_text, variable_text
+    var_text.set_text('')
+    return line, time_text, period_text, var_text
 
 def update(i):              # ここのiは下のframes=fに対応した引数になっている
     x_pos = [0, l + x[i]]
@@ -76,8 +76,8 @@ def update(i):              # ここのiは下のframes=fに対応した引数�
     line.set_data(x_pos,y_pos)
     time_text.set_text(time_template % (i*dt))
     period_text.set_text(period_template % period)
-    variable_text.set_text(variable_template)
-    return line, time_text, period_text, variable_text
+    var_text.set_text(var_template)
+    return line, time_text, period_text, var_text
 
 f = np.arange(0, len(t))
 frame_int = 1000 * dt       # [ms] interval between frames
